94a1282df65aaf426200905b7e8b8f6771177d42
JulienLemarchand <jl.julienlemarchand@gmail.com>
2018-07-23 14:39:35 +0200

    creation liste et detail article

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Articles;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -16,4 +17,42 @@ class ArticlesController extends Controller
             'controller_name' => 'ArticlesController',
         ]);
     }
+
+
+
+    /**
+     * @Route("/articles/", name="all-articles")
+     */
+    public function showAllArticles(){
+
+        $repository = $this->getDoctrine()->getRepository(Articles::class);
+
+        $articles = $repository->findAll();
+
+        return $this->render('articles/articles.html.twig', array('articles'=>$articles));
+
+    }
+
+
+
+    /**
+     * @Route("/article/", name="detail-article", requirements={"id"="[0-9]+"})
+     */
+    public function detail($id){
+
+        $repository = $this->getDoctrine()->getRepository(Articles::class);
+
+        $article = $repository->find($id);
+
+            if(!$article){
+                throw $this->createNotFoundException('No article found for id' .$id);
+            }
+
+        return $this->render('article/detail.html.twig',  array('article'=>$article));
+
+    }
+
+
+
+
 }
