c6e271f353e490184c553a969f4a494453d653a5
Spakye <leomagnac@gmail.com>
2018-07-24 11:54:59 +0200

    Inserer

--- src/Controller/SampleController.php
+++ src/Controller/SampleController.php
@@ -4,6 +4,12 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Entity\Tracks;
+use App\Entity\Artistes;
+use App\Entity\Albums;
+use App\Form\InsererTrackType;
+use App\Form\InsererAlbumType;
+use App\Form\InsererArtisteType;
 
 class SampleController extends Controller
 {
@@ -15,5 +21,35 @@ class SampleController extends Controller
         return $this->render('sample/index.html.twig', [
             'controller_name' => 'SampleController',
         ]);
+    }
+
+    /**
+    * @Route("/inserer", name="inserer")
+    */
+    public function inserer(){
+    	// $this->denyAccessunlessgranted('IS_AUTHENTICATED_FULLY');
+    	$trackSample = new Tracks();
+    	$albumSample = new Albums();
+    	$artisteSample = new Artistes();
+    	$formArSample = $this->createForm(InsererArtisteType::class, $artisteSample);
+    	$formAlSample = $this->createForm(InsererAlbumType::class, $albumSample);
+    	$formTrSample = $this->createForm(InsererTrackType::class, $trackSample);
+
+    	$trackSampleur = new Tracks();
+    	$albumSampleur = new Albums();
+    	$artisteSampleur = new Artistes();
+    	$formArSampleur = $this->createForm(InsererArtisteType::class, $artisteSampleur);
+    	$formAlSampleur = $this->createForm(InsererAlbumType::class, $albumSampleur);
+    	$formTrSampleur = $this->createForm(InsererTrackType::class, $trackSampleur);
+
+    	return $this->render('sample/inserer.html.twig',
+                                array('formArSample' => $formArSample->createview(),
+                            			'formAlSample'=>$formAlSample->createview(),
+                            			'formTrSample'=>$formTrSample->createview(),
+                            			'formArSampleur' => $formArSampleur->createview(),
+                            			'formAlSampleur'=>$formAlSampleur->createview(),
+                            			'formTrSampleur'=>$formTrSampleur->createview()
+                            		)
+            );
     }
 }
