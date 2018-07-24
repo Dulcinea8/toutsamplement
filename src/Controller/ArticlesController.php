01ea9f858570184da2fcfdb8fc85c0964c900e46
JulienLemarchand <jl.julienlemarchand@gmail.com>
2018-07-24 16:48:30 +0200

    Creation modif et suppression articles

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Form\ArticlesType;
 use App\Service\FileUploader;
 use App\Entity\Articles;
+use Symfony\Component\HttpFoundation\File\File;
 use App\Entity\Users;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -104,6 +105,89 @@ class ArticlesController extends Controller
         return $this->render('articles/addarticle.html.twig', array('form' => $form->createView()));
     }
 
+
+    /**
+     * @route("/article/update/{id}", name="modifier-article", requirements={"id"="\d+"})
+     *
+     */
+
+    public function updateArticle(Articles $article, Request $request, FileUploader $uploader){
+
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        //je stocke le nom du fichier
+        $fileName = $article->getImage();
+
+        if($article->getImage()) {
+
+            //pour pouvoir generer le formulaire, on doit transformer le nom du fichier stocké pour l'instant dans l'attribut
+            //image en instance de la classe File (ce qui est attendu par le formulaire)
+
+            $article->setImage(new File($this->getParameter('articles_image_directory') . '/' . $article->getImage()));
+
+        }
+
+        $form = $this->createForm(ArticlesType::class, $article);
+
+        $form->handleRequest($request);
+
+        //je vais faire le traitement d'ajout si le formulaire a etet envoyé et s'il est valide
+        if($form->isSubmitted() && $form->isvalid()){
+
+            //form->getData() contient les données envoyées
+            //ici on charge le formulaire de remplir notre objet categorie avec les données
+            $article = $form->getData();
+
+            //je ne fais le traitement d'upload que si on m'a envoyé un fichier
+            if($article->getImage()) {
+
+                //on recupere un objet de classe file
+                $file = $article->getImage();
+
+                //on genere un nouveau nom
+                $fileName = $uploader->upload($file, $fileName);
+            }
+
+
+            //jon met à jour la propriété image qui doit contenir le nom du fichier pour etre persistée
+            // fileName contient soit le nouveau nom de fichier si on a recu une nouvelle image,
+            // soit l'ancien si l'utilisateur n'a pas modifié l'image
+            $article->setImage($fileName);
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $entityManager->flush();
+
+            $this->addFlash('info', 'L\'article a bien été modifié');
+
+            return $this->redirectToRoute('all-articles');
+        }
+
+        return $this->render('articles/updatearticle.html.twig', array('form' => $form->createView()));
+
+    }
+
+    /**
+     * @route("article/delete/{id}", name="supprimer-article", requirements={"id"="\d+"})
+     */
+    public function deleteArticle(Articles $article){
+
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        //recuperation de l'entite manager
+        $entityManager = $this->getDoctrine()->getManager();
+
+        //je veux supprimer cette catégorie
+        $entityManager->remove($article);
+
+        //j'execute la requete
+        $entityManager->flush();
+
+        $this->addFlash('danger', 'L\'article a bien été supprimé');
+
+        return $this->redirectToRoute('all-articles');
+
+    }
 
 
 
