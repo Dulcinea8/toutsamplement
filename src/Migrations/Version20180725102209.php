42cd01d860d8264935ed86d4ec3e7e97c356993c
JulienLemarchand <jl.julienlemarchand@gmail.com>
2018-07-25 13:53:55 +0200

    changement page articles

--- src/Migrations/Version20180725102209.php
+++ src/Migrations/Version20180725102209.php
@@ -8,16 +8,16 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20180725092039 extends AbstractMigration
+final class Version20180725102209 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE relations (original_id INT NOT NULL, sample_id INT NOT NULL, INDEX IDX_146CBF78108B7592 (original_id), INDEX IDX_146CBF781B1FEA20 (sample_id), PRIMARY KEY(original_id, sample_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE relations (id INT AUTO_INCREMENT NOT NULL, sampleur_id INT NOT NULL, original_id INT NOT NULL, INDEX IDX_146CBF784E35BEC1 (sampleur_id), INDEX IDX_146CBF78108B7592 (original_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE relations ADD CONSTRAINT FK_146CBF784E35BEC1 FOREIGN KEY (sampleur_id) REFERENCES tracks (id)');
         $this->addSql('ALTER TABLE relations ADD CONSTRAINT FK_146CBF78108B7592 FOREIGN KEY (original_id) REFERENCES tracks (id)');
-        $this->addSql('ALTER TABLE relations ADD CONSTRAINT FK_146CBF781B1FEA20 FOREIGN KEY (sample_id) REFERENCES tracks (id)');
     }
 
     public function down(Schema $schema) : void
