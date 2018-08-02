2c6a552bf82069c75592e93866f746f5e724a965
dulcine8 <dulce.cesar263@gmail.com>
2018-08-02 09:02:03 +0200

    profilUserok

--- src/Migrations/Version20180801151243.php
+++ src/Migrations/Version20180801151243.php
@@ -8,16 +8,14 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20180801143916 extends AbstractMigration
+final class Version20180801151243 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE relations DROP FOREIGN KEY FK_146CBF784E35BEC1');
-        $this->addSql('DROP INDEX IDX_146CBF784E35BEC1 ON relations');
-        $this->addSql('ALTER TABLE relations DROP sampleur_id');
+        $this->addSql('ALTER TABLE relations CHANGE sampleur_id sampleur_id INT DEFAULT NULL, CHANGE original_id original_id INT DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -25,8 +23,6 @@ final class Version20180801143916 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE relations ADD sampleur_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE relations ADD CONSTRAINT FK_146CBF784E35BEC1 FOREIGN KEY (sampleur_id) REFERENCES tracks (id)');
-        $this->addSql('CREATE INDEX IDX_146CBF784E35BEC1 ON relations (sampleur_id)');
+        $this->addSql('ALTER TABLE relations CHANGE sampleur_id sampleur_id INT NOT NULL, CHANGE original_id original_id INT NOT NULL');
     }
 }
