33d503e609953cd7674dc0c53782ca2ea46d30c2
Dulcinea8 <dulce.cesar263@gmail.com>
2018-11-14 16:21:32 +0100

    verif form contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,49 +4,91 @@ namespace App\Controller;
 
 
 use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+
 class ContactController extends Controller
 {
+
     /**
      * @Route("/contact", name="contact")
+     *
      */
     public function index(\Swift_Mailer $mailer)
     {
-    	$confirmMessage="";
-    	$errorMessage="";
-    	if ($_POST) {
+            $confirmMessage="";
+            $errorMessage="";
+        if(!empty($_POST)){
 
-    		$nom= $_POST['nom'];
-    		$sujet= $_POST['sujet'];
-    		$email= $_POST['email'];
-    		$message= $_POST['message'];
+            $errors = [];
 
-    		$message = (new \Swift_Message('Contact tout samplement'))
-        ->setFrom($_POST['email'])
-        ->setTo('contact.toutsamplement@gmail.com')
-        ->setBody(
-        	$this->renderView(
-                // templates/emails/registration.html.twig
-                'contact/mail.html.twig',
-                ['nom'=>$nom, 'sujet'=>$sujet, 'email'=>$email, 'message'=>$message]
-            ),
-            'text/html'
-        
-        )
-        
-    ;
-    if ($mailer->send($message)) {
-    	$confirmMessage="L'email a bien été envoyé !";
-    }else{
-    	$errorMessage="Erreur lors de l'envoi de l'email veuillez verifier vos champs.";
-    }
+
+            if(empty($_POST['nom']) ){
+                $errors[] = 'Veuillez rensegner votre nom';
+            }
+            if(empty($_POST['sujet'])){
+                $errors[] = 'Veuillez rensegner un sujet';
+            }
+
+            //verifier l'email
+            if(empty($_POST['email']) OR filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) == false){
+                $errors[] = 'email erronné o manquante';
+            }
+
+
+            //verifier le message
+            if(empty($_POST['message'])){
+                $errors[] = 'Message manquante';
+            }
+
+
+            //vérifs terminées
+
+            if(empty($errors)){
+
+                $nom= $_POST['nom'];
+                $sujet= $_POST['sujet'];
+                $email= $_POST['email'];
+                $message= $_POST['message'];
+
+                $message = (new \Swift_Message('Contact tout samplement'))
+                    ->setFrom($_POST['email'])
+                    ->setTo('contact.toutsamplement@gmail.com')
+                    ->setBody(
+                        $this->renderView(
+                        // templates/emails/registration.html.twig
+                            'contact/mail.html.twig',
+                            ['nom'=>$nom, 'sujet'=>$sujet, 'email'=>$email, 'message'=>$message]
+                        ),
+                        'text/html'
+
+                    );
+
+                if ($mailer->send($message)) {
+                    $confirmMessage="L'email a bien été envoyé !";
+                }else{
+                    $errorMessage="Erreur lors de l'envoi de l'email veuillez verifier vos champs.";
+                }
+
+
+            }
+            else {
+
+
+                $errorMessage="Erreur lors de l'envoi de l'email veuillez verifier vos champs.";
+            }
+
+
+
     
     	}
 
+
+
         return $this->render('contact/index.html.twig', [
-            'success'=>$confirmMessage, 'error'=>$errorMessage
+            'success'=>$confirmMessage, 'error'=>$errorMessage,
         ]);
     }
 
